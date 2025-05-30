fe3e74ed7dbacd9123e4bad9372eea8011a9d6cd
Kostya <ckutls228@gmail.com>
2025-05-30 20:24:04 +0300

    added main page style

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,19 @@
 </script>
 
 <template>
-  <main>
-    <div class="w-full flex gap-5 p-5">
-      <LeaderboardTheLeaderBoard />
+  <main class="bg-[#FDFFFD] rounded-3xl m-8">
+    <div>
+      <img class="absolute right-0" src="@/assets/img/sun-img.png">
+      <div class="text-center pt-64">
+        <h1 class="text-[#050E01] opacity-50 font-bold text-7xl">feel the real</h1>
+        <h1 class="text-[#050E01] font-bold text-7xl ml-64">business</h1>
+      </div>
+      <img class="ml-32 -mt-24" src="@/assets/img/earth-img.png" >
+      <button class="bg-[#fdfffd33] left-1/2 -translate-x-1/2 ml-9 cursor-pointer text-[#FDFFFD] font-semibold text-3xl font-inter-tight -mt-40 flex items-center rounded-full py-5 px-10 absolute">
+        Create a business
+        <img src="@/assets/img/array-forward-icon.svg">
+      </button>
+      <img class="absolute top-1/2 left-64" src="@/assets/img/array-icon.svg" alt="">
     </div>
   </main>
 </template>
