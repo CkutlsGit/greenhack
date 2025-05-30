35c9bfdf624eddf9fe2b02f7b7a4e66378931773
Kostya <ckutls228@gmail.com>
2025-05-30 18:45:07 +0300

    added user-info request in the-header

--- components/header/TheHeader.vue
+++ components/header/TheHeader.vue
@@ -5,6 +5,18 @@ const router = useRouter()
 
 const url = ref('')
 const authToken = ref('')
+const userData = ref()
+
+const getUserInfo = async () => {
+  const responseUser = await $fetch(`${ config.public.apikey }/users/me`, {
+      method: 'GET',
+      headers: {
+        'Authorization': authToken.value
+      }
+    })
+
+    userData.value = responseUser
+}
 
 onMounted(async () => {
   authToken.value = GetCookie('authtoken')
@@ -14,15 +26,20 @@ onMounted(async () => {
     const response = await $fetch(`${ config.public.apikey }/auth/google/callback?code=${ route.query.code }`) 
 
     SetCookie('authtoken', response.authToken)
-    authToken.value = GetCookie('authtoken')
+    authToken.value = response.authToken
 
     await router.replace({ query: {} })
+
+    getUserInfo()
   }
   else {
     const response = await $fetch(`${ config.public.apikey }/auth/google/url`)
   
     url.value = response.url
   }
+  }
+  else {
+    getUserInfo()
   }
 })
 
@@ -44,9 +61,9 @@ const googleAuth = () => {
         <button @click="googleAuth" class="capitalize text-[#050E01] font-medium text-3xl font-inter-tight cursor-pointer">sign up</button>
         <button @click="googleAuth" class="capitalize text-[#F2F7F1] bg-[#050E01] py-5 px-10 rounded-full font-medium text-3xl font-inter-tight cursor-pointer">log in</button>
       </div>
-      <div v-else class="header__user flex items-center gap-6">
-        <h2 class="text-3xl text-[#050E01] font-medium font-inter-tight">Ckutls</h2>
-        <div class="w-14 h-14 bg-amber-950 rounded-xl"></div>
+      <div v-else-if="authToken && userData" class="header__user flex items-center gap-6">
+        <h2 class="text-3xl text-[#050E01] font-medium font-inter-tight">{{ userData.nickname }}</h2>
+        <img class="w-14 h-14 rounded-xl" :src="userData.avatarUrl" alt="User icon">
       </div>
     </nav>
   </header>
