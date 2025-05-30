a3abaa5cdd64e0ac11d84b1ad234993df82edc8f
Kostya <ckutls228@gmail.com>
2025-05-30 16:54:49 +0300

    added leaderboard in main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,54 @@
-<template>
-  <div>
-    <h1>123123</h1>
-  </div>
-</template>
-
-<script lang="ts" setup>
+<script setup>
 
 </script>
+
+<template>
+  <main>
+    <div class="w-full flex">
+      <article class="bg-white rounded-lg p-5 shadow-md  min-h-screen">
+        <h2 class="text-xl mb-5 font-bold">Leaderboard</h2>
+        <div class="grid grid-cols-[50px_1fr_80px_50px] gap-x-2.5 py-2.5 border-b border-gray-200 items-center font-bold">
+          <div class="overflow-hidden text-ellipsis whitespace-nowrap text-left">Rank</div>
+          <div class="overflow-hidden text-ellipsis whitespace-nowrap text-left">Username</div>
+          <div class="overflow-hidden text-ellipsis whitespace-nowrap text-left">Points</div>
+          <div class="overflow-hidden text-ellipsis whitespace-nowrap text-left">
+            <img src="/assets/img/icon-green.svg" alt="Green icon" class="block w-5 h-5">
+          </div>
+        </div>
+        <div class="grid grid-cols-[50px_1fr_80px_50px] gap-x-2.5 py-2.5 border-b border-gray-200 items-center">
+          <div class="overflow-hidden text-ellipsis whitespace-nowrap text-left">#1</div>
+          <div class="overflow-hidden text-ellipsis whitespace-nowrap text-left">Roma</div>
+          <div class="overflow-hidden text-ellipsis whitespace-nowrap text-left">6167</div>
+          <div class="overflow-hidden text-ellipsis whitespace-nowrap text-left">43</div>
+        </div>
+        <div class="grid grid-cols-[50px_1fr_80px_50px] gap-x-2.5 py-2.5 border-b border-gray-200 items-center">
+          <div class="overflow-hidden text-ellipsis whitespace-nowrap text-left">#1</div>
+          <div class="overflow-hidden text-ellipsis whitespace-nowrap text-left">Roma</div>
+          <div class="overflow-hidden text-ellipsis whitespace-nowrap text-left">6167</div>
+          <div class="overflow-hidden text-ellipsis whitespace-nowrap text-left">43</div>
+        </div>
+        <div class="grid grid-cols-[50px_1fr_80px_50px] gap-x-2.5 py-2.5 border-b border-gray-200 items-center last:border-b-0">
+          <div class="overflow-hidden text-ellipsis whitespace-nowrap text-left">#1</div>
+          <div class="overflow-hidden text-ellipsis whitespace-nowrap text-left">Roma</div>
+          <div class="overflow-hidden text-ellipsis whitespace-nowrap text-left">6167</div>
+          <div class="overflow-hidden text-ellipsis whitespace-nowrap text-left">43</div>
+        </div>
+        <div class="grid grid-cols-[50px_1fr_80px_50px] gap-x-2.5 py-2.5 border-b border-gray-200 items-center last:border-b-0">
+          <div class="overflow-hidden text-ellipsis whitespace-nowrap text-left">#1</div>
+          <div class="overflow-hidden text-ellipsis whitespace-nowrap text-left">Roma</div>
+          <div class="overflow-hidden text-ellipsis whitespace-nowrap text-left">6167</div>
+          <div class="overflow-hidden text-ellipsis whitespace-nowrap text-left">43</div>
+        </div>
+        <div class="grid grid-cols-[50px_1fr_80px_50px] gap-x-2.5 py-2.5 border-b border-gray-200 items-center last:border-b-0">
+          <div class="overflow-hidden text-ellipsis whitespace-nowrap text-left">#1</div>
+          <div class="overflow-hidden text-ellipsis whitespace-nowrap text-left">Romaasdasddasdas</div>
+          <div class="overflow-hidden text-ellipsis whitespace-nowrap text-left">6167</div>
+          <div class="overflow-hidden text-ellipsis whitespace-nowrap text-left">43</div>
+        </div>
+      </article>
+    </div>
+  </main>
+</template>
 
 <style>
 
