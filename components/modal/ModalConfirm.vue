afabb7fa52b347e2ec5620cb79721ccf6aa26e23
Kostya <ckutls228@gmail.com>
2025-05-31 11:20:32 +0300

    added test button changed styled

--- components/modal/ModalConfirm.vue
+++ components/modal/ModalConfirm.vue
@@ -25,23 +25,23 @@ const startGameFunction = async () => {
 </script>
 
 <template>
-  <div class="bg-[#050E010D] absolute left-1/2 -translate-x-1/2 w-full h-full overflow-y-hidden mx-auto z-[10000] flex justify-center">
-    <div class="bg-[#FDFFFD] w-[600px] h-[420px] p-6 rounded-2xl mt-32">
+  <div class="bg-[#050E010D] absolute mt-32 inset-0 w-full h-full overflow-y-hidden z-[10000] flex flex-col items-center">
+    <div class="bg-[#FDFFFD] w-[600px] h-[420px] p-6 rounded-2xl mt-32 relative mx-auto">
       <h1 class="text-4xl font-collvetica text-[#050E01]">{{ mainStore.projectName }}</h1>
       <div class="flex items-center mt-6">
         <h2 class="text-[#050E01] opacity-50 font-semibold font-inter-tight text-2xl">Location</h2>
         <h2 class="ml-4 text-[#050E01] font-semibold font-inter-tight text-2xl">{{ mainStore.location }}</h2>
       </div>
       <div class="flex items-start mt-6">
-        <h2 class="text-[#050E01] opacity-50 font-semibold font-inter-tight text-2xl">Main Idea</h2>
+        <h2 class="text-[#050E01] opacity-50 font-semibold font-inter-tight text-2xl max-w-full break-all">Main Idea</h2>
         <h2 class="ml-4 text-[#050E01] font-semibold font-inter-tight text-2xl">
           {{ mainStore.idea }}
         </h2>
       </div>
-      <div class="flex items-center mt-36">
-        <img @click="$emit('closeModal')" class="bg-[#050E011A] rounded-full p-5 mr-5 cursor-pointer" src="/public/icons/array-back-icon.svg" />
-        <button @click="startGameFunction" :class="{ 'bg-[#050E01] opacity-30' : loading }" :disabled="loading" class="font-inter-tight font-semibold text-[#FDFFFD] text-3xl bg-[#2CAE28] rounded-full p-5 w-full cursor-pointer">Start a journey</button>
-      </div>
+    </div>
+    <div class=" absolute bottom-50 w-[600px] bg-[#FDFFFD] py-6 rounded-full px-10 z-[100000] flex items-center justify-center">
+      <img @click="$emit('closeModal')" class="bg-[#050E011A] rounded-full p-5 mr-5 cursor-pointer" src="/public/icons/array-back-icon.svg" />
+      <button @click="startGameFunction" :class="{ 'bg-[#050E01] opacity-30' : loading }" :disabled="loading" class="font-inter-tight font-semibold text-[#FDFFFD] text-3xl bg-[#2CAE28] rounded-full p-5 w-full cursor-pointer">Start a journey</button>
     </div>
   </div>
   <LoaderItemAi v-if="loading" />
